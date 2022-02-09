57694256fe571e399dec66036b6a7b49cf4b3d51
leoviera <99355533+leoviera@users.noreply.github.com>
2022-02-09 14:58:39 -0300

    upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+box = createSprites(200,200,20,20);
+
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW)) {
+ box.position.x = box.position.x +5;
+
+
+  }
+  drawSprites();
+}
+
+
+
+
+
